67ea707fa69143d5b068137cc4676696f8f06053
Royal <kingstars@gmail.com>
2024-09-01 15:53:42 -0400

    solution for boxed-data

--- content/mods/A-foundations/topics/smart-pointers/exercises/boxed-data/src/main.rs
+++ content/mods/A-foundations/topics/smart-pointers/exercises/boxed-data/src/main.rs
@@ -6,6 +6,7 @@
 /// - Add support for multiplication and division
 ///
 /// - We have added the form "Summation(Vec<Expr>)", representing the sum of a list of expressions.
+///
 /// Question: why can we get away with Vec<Expr> enough in that case, instead of Box<Vec<Expr>> ?
 ///
 /// - EXTRA: Since division can fail, the function eval needs to return an Option<i64>, where None indicates that a division by
@@ -18,48 +19,59 @@ enum Expr {
     Sub(Box<Expr>, Box<Expr>),
     Var,
     Summation(Vec<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+    Sigma(Box<Expr>, Box<Expr>),
 }
 
 // inject these two identifiers directly into the current namespace
-use Expr::Const;
-use Expr::Summation;
-use Expr::Var;
+use Expr::*;
 
 // These are convenience functions, so you don't have to type "Box::new" as often
 // when building test-data types
 fn add(x: Expr, y: Expr) -> Expr {
-    Expr::Add(Box::new(x), Box::new(y))
+    Add(Box::new(x), Box::new(y))
 }
 
 fn sub(x: Expr, y: Expr) -> Expr {
-    Expr::Sub(Box::new(x), Box::new(y))
+    Sub(Box::new(x), Box::new(y))
 }
 
 fn mul(x: Expr, y: Expr) -> Expr {
-    todo!()
+    Mul(Box::new(x), Box::new(y))
 }
 
 fn div(x: Expr, y: Expr) -> Expr {
-    todo!()
+    Div(Box::new(x), Box::new(y))
 }
 
 // ...
 
-fn eval(expr: &Expr, var: i64) -> i64 {
-    // this should return an Option<i64>
-    use Expr::*;
+fn eval(expr: &Expr, var: i64) -> Option<i64> {
     match expr {
-        Const(k) => *k,
-        Var => var,
-        Add(lhs, rhs) => eval(lhs, var) + eval(rhs, var),
-        Sub(lhs, rhs) => eval(lhs, var) - eval(rhs, var),
-
+        Const(k) => Some(*k),
+        Var => Some(var),
+        Add(lhs, rhs) => eval(lhs, var).and_then(|l| eval(rhs, var).and_then(|r| l.checked_add(r))),
+        Sub(lhs, rhs) => eval(lhs, var).and_then(|l| eval(rhs, var).and_then(|r| l.checked_sub(r))),
+        Mul(lhs, rhs) => eval(lhs, var).and_then(|l| eval(rhs, var).and_then(|r| l.checked_mul(r))),
+        Div(lhs, rhs) => eval(lhs, var).and_then(|l| eval(rhs, var).and_then(|r| l.checked_div(r))),
         Summation(exprs) => {
-            let mut acc = 0;
+            let mut acc: i64 = 0;
             for e in exprs {
-                acc += eval(e, var);
+                acc = acc.checked_add(eval(e, var)?)?;
             }
-            acc
+            Some(acc)
+        }
+        Sigma(from, to) => {
+            let from_val = eval(from, var)?;
+            let to_val = eval(to, var)?;
+            let mut exprs = Vec::new();
+            let mut val = from_val;
+            while val <= to_val {
+                exprs.push(Const(val));
+                val = val.checked_add(1)?;
+            }
+            eval(&Summation(exprs), var)
         }
     }
 }
@@ -68,7 +80,7 @@ fn main() {
     let test = |expr| {
         let value = rand::random::<i8>() as i64;
         println!(
-            "{:?} with Var = {} ==> {}",
+            "{:?} with Var = {} ==> {:?}",
             &expr,
             value,
             eval(&expr, value)
@@ -80,7 +92,11 @@ fn main() {
     test(sub(Var, Const(5)));
     test(sub(Var, Var));
     test(add(sub(Var, Const(5)), Const(5)));
+    test(mul(Var, Const(12)));
+    test(div(Var, Const(0)));
+    test(div(Var, Const(10)));
     test(Summation(vec![Var, Const(1)]));
+    test(Sigma(Box::new(Const(1)), Box::new(Const(5))));
 }
 
 #[cfg(test)]
@@ -90,12 +106,19 @@ mod test {
     #[test]
     fn test_cases() {
         let x = 42;
-        assert_eq!(eval(&Const(5), x), 5);
-        assert_eq!(eval(&Var, x), 42);
-        assert_eq!(eval(&sub(Var, Const(5)), x), 37);
-        assert_eq!(eval(&sub(Var, Var), x), 0);
-        assert_eq!(eval(&add(sub(Var, Const(5)), Const(5)), x), 42);
-        assert_eq!(eval(&Summation(vec![Var, Const(1)]), x), 43);
+        assert_eq!(eval(&Const(5), x), Some(5));
+        assert_eq!(eval(&Var, x), Some(42));
+        assert_eq!(eval(&sub(Var, Const(5)), x), Some(37));
+        assert_eq!(eval(&sub(Var, Var), x), Some(0));
+        assert_eq!(eval(&add(sub(Var, Const(5)), Const(5)), x), Some(42));
+        assert_eq!(eval(&mul(Var, Const(10)), x), Some(420));
+        assert_eq!(eval(&div(Var, Const(0)), x), None);
+        assert_eq!(eval(&div(Const(84), Var), x), Some(2));
+        assert_eq!(eval(&Summation(vec![Var, Const(1)]), x), Some(43));
+        assert_eq!(
+            eval(&Sigma(Box::new(Const(1)), Box::new(Const(5))), x),
+            Some(15)
+        );
     }
 }
 
