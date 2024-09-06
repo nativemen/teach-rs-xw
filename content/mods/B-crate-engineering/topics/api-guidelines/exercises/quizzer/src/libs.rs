22cebbe6a1061e1c4f2a80a70c10f3a5304aa0fd
Royal <kingstars@gmail.com>
2024-09-06 09:47:56 -0400

    solution fix for quizzer

--- content/mods/B-crate-engineering/topics/api-guidelines/exercises/quizzer/src/libs.rs
+++ content/mods/B-crate-engineering/topics/api-guidelines/exercises/quizzer/src/libs.rs
@@ -3,21 +3,21 @@ use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Deserialize, Serialize)]
 pub struct QuizQuestion {
     pub question: String,
     pub options: [String; 4],
     pub correct_answer: usize,
 }
 
-pub fn save_questions(questions: &[QuizQuestion], filename: &str) -> Result<(), anyhow::Error> {
+pub fn save_questions(questions: &[QuizQuestion], filename: &str) -> Result<()> {
     let file = File::create(filename)?;
     let writer = BufWriter::new(file);
-    serde_json::to_writer(writer, &questions)?;
+    serde_json::to_writer(writer, questions)?;
     Ok(())
 }
 
-pub fn load_questions(filename: &str) -> Result<Vec<QuizQuestion>, anyhow::Error> {
+pub fn load_questions(filename: &str) -> Result<Vec<QuizQuestion>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let questions = serde_json::from_reader(reader)?;
--- content/mods/B-crate-engineering/topics/api-guidelines/exercises/quizzer/src/main.rs
+++ content/mods/B-crate-engineering/topics/api-guidelines/exercises/quizzer/src/main.rs
@@ -1,10 +1,19 @@
 use anyhow::Result;
 use clap::Command;
 use quizzer::*;
-use std::io::{self};
+use std::io;
+
+const FILENAME: &str = "questions.json";
 
 fn main() -> Result<()> {
     let matches = Command::new("Quizzer")
+        .about("questions & quiz")
+        .version("v1.0")
+        .author("nativemen")
+        .arg_required_else_help(true)
+        .help_template(
+            "{bin} {version}\n{author}\n{about}\n\nUSAGE:\n    {usage}\n\nOPTIONS:\n{all-args}",
+        )
         .subcommand(Command::new("enter").about("Enter quiz questions"))
         .subcommand(Command::new("quiz").about("Take a quiz"))
         .get_matches();
@@ -20,19 +29,19 @@ fn enter_questions() -> Result<()> {
     let mut questions = Vec::new();
     loop {
         let mut question = String::new();
-        println!("Enter the question:");
+        println!("Please enter the question:");
         io::stdin().read_line(&mut question)?;
         question = question.trim().to_string();
 
         let mut options: [String; 4] = Default::default();
         for (i, option) in options.iter_mut().enumerate() {
-            println!("Enter option {}:", i + 1);
+            println!("Please enter option {}:", i + 1);
             io::stdin().read_line(option)?;
             *option = option.trim().to_string();
         }
 
         let mut correct_answer = String::new();
-        println!("Enter the number of the correct answer:");
+        println!("Please enter the number of the correct answer:");
         io::stdin().read_line(&mut correct_answer)?;
         let correct_answer = correct_answer.trim().parse::<usize>()?;
 
@@ -42,20 +51,21 @@ fn enter_questions() -> Result<()> {
             correct_answer,
         });
 
-        println!("Do you want to add another question? (yes/no)");
         let mut another = String::new();
+        println!("Do you want to add another question? (yes/no)");
         io::stdin().read_line(&mut another)?;
         if another.trim().to_lowercase() != "yes" {
             break;
         }
     }
 
-    save_questions(&questions, "questions.json")?;
+    save_questions(&questions, FILENAME)?;
     Ok(())
 }
+
 fn take_quiz() -> Result<()> {
-    let questions = load_questions("questions.json")?;
     let mut score = 0;
+    let questions = load_questions(FILENAME)?;
     for (i, q) in questions.iter().enumerate() {
         println!("Question {}: {}", i + 1, q.question);
         for (j, option) in q.options.iter().enumerate() {
@@ -66,13 +76,11 @@ fn take_quiz() -> Result<()> {
         println!("Please input your answer:");
         io::stdin().read_line(&mut answer)?;
         let answer = answer.trim().parse::<usize>()?;
-
         if answer == q.correct_answer {
             score += 1;
         }
     }
 
     println!("Your scores is {} / {}", score, questions.len());
-
     Ok(())
 }
